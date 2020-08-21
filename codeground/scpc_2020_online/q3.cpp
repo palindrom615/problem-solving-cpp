39133e1633f3b85b9d9d4c1963667a4e1418bf15
Jang Whemoon <palindrom615@gmail.com>
2020-08-22 07:34:50 +0900

    Add q3

--- codeground/scpc_2020_online/q3.cpp
+++ codeground/scpc_2020_online/q3.cpp
@@ -9,36 +9,100 @@ Please be very careful.
 */
 /**
  * 어떤 점이 사다리를 타고 움직인다고 해 보자. 하나는 출발지에서 아래로, 하나는 도착지에서 위로.
- * w.r.t some column(s) of ladder, 두 점 모두 어느 시점에선가 이 칼럼의 각각 위쪽과 아래쪽을 지난다.
- * 그렇지 않으면 rung을 아무리 제거하더라도 둘은 만나지 못한다.
+ * w.r.t some pillar(s) of ladder, 두 점 모두 어느 시점에선가 이 세로선의 각각 위쪽과 아래쪽을 지난다.
+ * 그렇지 않으면 이음선을 아무리 제거하더라도 둘은 만나지 못한다.
  *
- * column을 rung으로 나뉜 구간의 집합이라고 하면 점이 지나다니는 자취 구간의 연속으로 나타낼 수 있다.
- * 한 column 안에서 두 점의 자취가 포함된 가장 가까운 구간을 구하는 것이 문제.
+ * 점이 움직이는 것을 '몇 번째 세로선의(pillarNum) 몇 번째 이음선(stepIdx)에 도착'의 연속으로 표현.
  *
- * 각 column의 구간의 개수를 integer로 나타내면?
- * rung 하나가 갈 때 마다 그 양쪽의 column의 구간의 개수가 하나씩 늘어난다..!
+ * ex. 그림의 경우
+ * 1st ladder_down: (4, 2) -> (2, 3)
+ * 3rd up: (4, 4) -> (1, 1)
+ * 4번째 세로선에서 1 down은 2번째, 3 up은 4번째 이음선에 도착하므로 그 사이에 있는 (4, 3) 이음선을 치우면 서로 만남을 알 수 있다.
  *
- * 자취를 몇 번째 rung을 넘었는지로 판단 가능.
- * 점의 자취를
- * * from(rung을 넘기 전 column)
- * * to(넘은 후 column)
- * * rung_idx(전체 rung 중 해당 rung의 idx)
- * 이렇게 세 값으로 구성된 구간의 array로 표현 가능.
- * 두 점이 만나기 위해 고장나야 하는 rung의 개수 = 두 점의 자취의 구간 중 to가 같고 rung_idx의 차이가 최소가 되는 구간 사이의 rung의 개수
- * (단, down 점의 rung_idx가 up 점의 rung_idx보다 커야 한다)
+ * pseudo code
+ *
+ * for (from, to)
+ * 		Answer += closest(pillarNum_Idxes(ladder_down(from)), pillarNum_Idxes(up(to)))
+ *
  */
 
 #include <iostream>
 #include <vector>
+#include <array>
 
 using namespace std;
 
 int Answer;
 
-typedef pair<int, int> Rung;
+typedef pair<int, int> Step;
+typedef pair<int, int> PillarNumStepIdx;
 
-int solve(int from, int to, vector<Rung>& rungs) {
-	return 0;
+vector<array<PillarNumStepIdx, 2>> get_step_position_on_pillar(vector<Step> &steps, int k, vector<int> &cnt) {
+	vector<array<PillarNumStepIdx, 2>> pillar_idx(k + 1);
+	for (int i = 1; i <= k; i++) {
+		auto step = steps[i];
+		pillar_idx[i] = {PillarNumStepIdx(step.first, cnt[step.first]), PillarNumStepIdx(step.second, cnt[step.second])};
+		cnt[step.first] += 1;
+		cnt[step.second] += 1;
+	}
+	return pillar_idx;
+}
+
+vector<PillarNumStepIdx> ladder_down(int pillar, vector<array<PillarNumStepIdx, 2>> &steps) {
+	vector<PillarNumStepIdx> res = {PillarNumStepIdx(pillar, 0)};
+	int locus = pillar;
+	for (auto &step: steps) {
+		if (step[0].first == locus) {
+			locus = step[1].first;
+			res.push_back(step[1]);
+		} else if (step[1].first == locus) {
+			locus = step[0].first;
+			res.push_back(step[0]);
+		}
+	}
+	return res;
+}
+
+vector<PillarNumStepIdx> ladder_up(int pillar, vector<array<PillarNumStepIdx, 2>> &steps, vector<int> &cnt) {
+	vector<PillarNumStepIdx> res = {PillarNumStepIdx (pillar, cnt[pillar])};
+	int locus = pillar;
+	for (auto step = steps.rbegin(); step != steps.rend(); step++) {
+		if ((*step)[0].first == locus) {
+			locus = (*step)[1].first;
+			res.push_back((*step)[1]);
+		} else if ((*step)[1].first == locus) {
+			locus = (*step)[0].first;
+			res.push_back((*step)[0]);
+		}
+	}
+	return res;
+}
+
+int solve(int from, int to, vector<array<PillarNumStepIdx, 2>> &steps, vector<int>& cnt) {
+	auto down_route = ladder_down(from, steps);
+	auto up_route = ladder_up(to, steps, cnt);
+	if (down_route.back().first == to) {
+		return 0;
+	}
+	int answer = 2001;
+	for (auto &dr: down_route) {
+		for (auto &ur: up_route) {
+			if (dr.first == ur.first) {
+				if (dr.second == ur.second) {
+					return 1;
+				}
+				if (dr.second < ur.second && (ur.second - dr.second - 1) < answer) {
+					answer = (ur.second - dr.second - 1);
+				} else {
+					break;
+				}
+			}
+		}
+	}
+	if (answer == 2001) {
+		return -1;
+	}
+	return answer;
 }
 
 int main(int argc, char **argv) {
@@ -65,16 +129,20 @@ int main(int argc, char **argv) {
 		 */
 		int N, k, m;
 		cin >> N >> k >> m;
-		vector<Rung> rungs(2000);
-		for (int i = 0; i < k; i++) {
+		vector<Step> steps(2001);
+
+		for (int i = 1; i <= k; i++) {
 			int a, b;
 			cin >> a >> b;
-			rungs[i] = Rung(a, b);
+			steps[i] = Step(a, b);
 		}
+		vector<int> cnt(k + 1, 1);
+
+		auto pillarNumStepIdxes = get_step_position_on_pillar(steps, k, cnt);
 		for (int i = 0; i < m; i++) {
 			int from, to;
 			cin >> from >> to;
-			Answer += solve(from, to, rungs);
+			Answer += solve(from, to, pillarNumStepIdxes, cnt);
 		}
 		/////////////////////////////////////////////////////////////////////////////////////////////
 
