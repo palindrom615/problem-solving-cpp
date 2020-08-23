93d38ebd64160fbdc46bcdfc128f874e30f0d371
Jang Whemoon <palindrom615@gmail.com>
2020-08-23 19:38:12 +0900

    Add test

---
+++ test/test.h
@@ -0,0 +1,11 @@
+//
+// Created by jwm on 8/23/20.
+//
+
+#ifndef PROBLEM_SOLVING_CPP_TEST_H
+#define PROBLEM_SOLVING_CPP_TEST_H
+
+#endif //PROBLEM_SOLVING_CPP_TEST_H
+
+int executable_main(int argc, char **argv);
+int executable_main();
